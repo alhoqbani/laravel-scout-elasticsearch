1bb1aa2e0a29a24213e9ee688669f63f12e86ae8
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-10-16 08:56:40 -0700

    add Service Provider

---
+++ src/ScoutElasticServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Alhoqbani\Elastic;
+
+use Elasticsearch\ClientBuilder;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Scout\EngineManager;
+
+class ScoutElasticServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->app->make(EngineManager::class)->extend('elastic', function () {
+
+            $hosts = $this->app['config.services.scout-elastic.hosts'];
+
+            $client = ClientBuilder::create()
+                ->setHosts($hosts)
+                ->build();
+
+            return new ScoutElasticEngine($client);
+        });
+
+        $this->publishes([
+            __DIR__ . '/../config/scout-elastic.php' => $this->app['path.config'] . DIRECTORY_SEPARATOR . 'scout-elastic.php',
+        ]);
+    }
+}
