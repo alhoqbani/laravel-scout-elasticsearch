f0d6a5546e970fd84de5c020a9844f9c33d0b75c
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-10-18 04:24:07 -0700

    test correct index with pagination

--- tests/PaginationTest.php
+++ tests/PaginationTest.php
@@ -18,12 +18,41 @@ class PaginationTest extends AbstractTestCase
         $client->shouldReceive('search')->with(\Mockery::subset([
             'body' => [
                 'size' => 13,
-                'from' => 39
-            ]
+                'from' => 39,
+            ],
         ]));
 
         $engine = new ScoutElasticEngine($client);
         $builder = new Builder(new ElasticTestModel, 'search term');
         $engine->paginate($builder, 13, 4);
+    }
+
+    public function test_paginate_will_send_correct_index_and_type_params()
+    {
+        $model = new ElasticTestModel;
+        $client = Mockery::mock(Client::class);
+        $client->shouldReceive('search')->with(\Mockery::subset([
+            'index' => $model->searchableAs(),
+            'type'  => $model->searchableAs(),
+        ]));
+
+        $engine = new ScoutElasticEngine($client);
+        $builder = new Builder($model, 'search term');
+        $engine->paginate($builder, 13, 4);
+    }
+
+    public function test_paginate_will_send_correct_index_and_type_params_when_custom_index_is_provided()
+    {
+        $model = new ElasticTestModel;
+        $client = Mockery::mock(Client::class);
+        $client->shouldReceive('search')->with(\Mockery::subset([
+            'index' => 'custom_index',
+            'type'  => 'custom_index',
+        ]));
+
+        $engine = new ScoutElasticEngine($client);
+        $builder = new Builder($model, 'search term');
+        $builder->within('custom_index');
+        $engine->paginate($builder, 10, 1);
     }
 }
